8e1d76846111082ca2457007de32becd46dc9247
서환수[Article FE Tech] <hs.seoo@navercorp.com>
2025-03-08 23:44:09 +0900

    메인 1차

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,12 @@ import people from "../assets/people.png?url";
 
 const Main = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-start">
+    <div
+      className="w-screen flex justify-center items-start"
+      style={{
+        height: "95vh",
+      }}
+    >
       <div className="bg-primary w-full h-full max-w-120 relative">
         <img
           src={flowers}
@@ -25,15 +30,30 @@ const Main = () => {
             minHeight: "350px",
           }}
         />
-        <div>
-          {/* <img
+        <div className="title relative flex flex-col items-center justify-start top-16 ">
+          <img
+            src={title}
+            className=""
+            style={{
+              width: "245px",
+              height: "95px",
+            }}
+          />
+          <p className="font-name pt-2">환수 & 예진</p>
+        </div>
+        <div className="info absolute flex flex-col items-center justify-center text-center bottom-15">
+          <img
             src={people}
-            className="absolute z-0 w-full h-full top-10 left-0 p-8"
+            className="z-20 w-full top-10 left-0 pr-11 pl-11"
             style={{
               minWidth: "180px",
               minHeight: "350px",
             }}
-          /> */}
+          />
+          <p className="font-normal pt-5">
+            2025년 6월 7일 토요일 오후 6시 30분
+          </p>
+          <p className="font-normal">로프트가든344 8층 메인홀</p>
         </div>
       </div>
     </div>
