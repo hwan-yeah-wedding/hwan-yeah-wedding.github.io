e3f80926731995b9205264610bd1c6029c2e6f75
서환수 <seohs7068@gmail.com>
2025-03-30 20:52:19 +0900

    Contact

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,5 +1,122 @@
-const Account = () => {
-  return <div className='w-screen h-screen bg-yellow-100 flex justify-center items-center'>Account</div>
-}
+import contact from "../assets/contact.png?url";
 
-export default Account
+const Account = () => {
+  return (
+    <div className="min-h-screen flex items-start justify-center">
+      <div className="bg-white w-full max-w-md p-6 ">
+        <div className="mt-13 flex items-center justify-center">
+          <img src={contact} className="title-contact" />
+        </div>
+        <div className="mb-13 mt-13">
+          <h2 className="text-xl font-bold mb-4">신랑측 계좌번호</h2>
+          <div className="table w-full">
+            <div className="table-row">
+              <div className="table-cell p-2 align-middle text-gray-400">
+                신 랑
+              </div>
+              <div className="table-cell p-2 align-middle font-bold">
+                서환수
+              </div>
+              <div className="table-cell p-2 align-middle">
+                은행이름 0000-000-000000
+              </div>
+              <div className="table-cell p-2 align-middle">
+                <button className="bg-parking px-3 py-1 rounded text-sm">
+                  복사
+                </button>
+              </div>
+            </div>
+            <div className="table-row">
+              <div className="table-cell p-2 align-middle text-gray-400">
+                아버지
+              </div>
+              <div className="table-cell p-2 align-middle font-bold">
+                서만근
+              </div>
+              <div className="table-cell p-2 align-middle">
+                은행이름 0000-000-000000
+              </div>
+              <div className="table-cell p-2 align-middle">
+                <button className="bg-parking px-3 py-1 rounded text-sm">
+                  복사
+                </button>
+              </div>
+            </div>
+            <div className="table-row">
+              <div className="table-cell p-2 align-middle text-gray-400">
+                어머니
+              </div>
+              <div className="table-cell p-2 align-middle font-bold">
+                유승주
+              </div>
+              <div className="table-cell p-2 align-middle">
+                은행이름 0000-000-000000
+              </div>
+              <div className="table-cell p-2 align-middle">
+                <button className="bg-parking px-3 py-1 rounded text-sm">
+                  복사
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <h2 className="text-xl font-bold mb-4">신부측 계좌번호</h2>
+          <div className="table w-full">
+            <div className="table-row">
+              <div className="table-cell p-2 align-middle text-gray-400">
+                신 부
+              </div>
+              <div className="table-cell p-2 align-middle font-bold">
+                채예진
+              </div>
+              <div className="table-cell p-2 align-middle">
+                은행이름 0000-000-000000
+              </div>
+              <div className="table-cell p-2 align-middle">
+                <button className="bg-parking px-3 py-1 rounded text-sm">
+                  복사
+                </button>
+              </div>
+            </div>
+            <div className="table-row">
+              <div className="table-cell p-2 align-middle text-gray-400">
+                아버지
+              </div>
+              <div className="table-cell p-2 align-middle font-bold">
+                채래문
+              </div>
+              <div className="table-cell p-2 align-middle">
+                은행이름 0000-000-000000
+              </div>
+              <div className="table-cell p-2 align-middle">
+                <button className="bg-parking px-3 py-1 rounded text-sm">
+                  복사
+                </button>
+              </div>
+            </div>
+            <div className="table-row">
+              <div className="table-cell p-2 align-middle text-gray-400">
+                어머니
+              </div>
+              <div className="table-cell p-2 align-middle font-bold">
+                나희숙
+              </div>
+              <div className="table-cell p-2 align-middle">
+                은행이름 0000-000-000000
+              </div>
+              <div className="table-cell p-2 align-middle">
+                <button className="bg-parking px-3 py-1 rounded text-sm">
+                  복사
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Account;
