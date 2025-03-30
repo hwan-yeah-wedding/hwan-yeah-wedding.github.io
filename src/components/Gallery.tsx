82f513645b9c32809761758e234202166008110d
서환수 <seohs7068@gmail.com>
2025-03-30 20:22:38 +0900

    Gallery 임시

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,34 @@
-const Gallery = () => {
-  return <div className='w-screen h-screen bg-yellow-100 flex justify-center items-center'>Gallery</div>
-}
+import flowers from "../assets/gallery_flowers.png?url";
+import gallery from "../assets/gallery.png?url";
 
-export default Gallery
+const Gallery = () => {
+  return (
+    <div className="w-screen flex justify-center items-start">
+      <img
+        src={flowers}
+        className="absolute w-full object-contain z-10 max-w-full"
+        style={{
+          minWidth: "225px",
+        }}
+      />
+      <div className="bg-secondary h-full w-full max-w-120 title relative flex flex-col items-center justify-start">
+        <div className="pt-10">
+          <img src={gallery} className="title-gallery" />
+        </div>
+        <div className="container mx-auto grid grid-cols-3 gap-2 px-2 pt-13 pb-2 md:grid-cols-3 z-10">
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+          <div className="w-full h-45 bg-gray-300 rounded"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
