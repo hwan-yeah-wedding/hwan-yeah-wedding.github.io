44c30989ca8cc75d2dc6616a3366dedb2e8661b3
서환수[Article FE Tech] <hs.seoo@navercorp.com>
2025-03-30 19:37:29 +0900

    Location 페이지

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -7,63 +7,90 @@ const LNG = 126.8754857;
 const Location = () => {
   const ref = useRef<HTMLDivElement>(null);
 
-  // useEffect(() => {
-  //   const map = new naver.maps.Map("map", {
-  //     center: new naver.maps.LatLng(LAT, LNG),
-  //     zoom: 17,
-  //   });
+  useEffect(() => {
+    const map = new naver.maps.Map("map", {
+      center: new naver.maps.LatLng(LAT, LNG),
+      zoom: 17,
+    });
 
-  //   const marker = new naver.maps.Marker({
-  //     position: new naver.maps.LatLng(LAT, LNG),
-  //     map: map,
-  //     icon: {
-  //       url: "https://map.pstatic.net/resource/api/v2/image/maps/selected-marker/229169@2x.png?version=12&mapping=marker-113",
-  //       scaledSize: [46, 59],
-  //     },
-  //     // animation: naver.maps.Animation.BOUNCE,
-  //   });
+    const marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(LAT, LNG),
+      map: map,
+      icon: {
+        url: "https://map.pstatic.net/resource/api/v2/image/maps/selected-marker/229169@2x.png?version=12&mapping=marker-113",
+        scaledSize: [46, 59],
+      },
+      // animation: naver.maps.Animation.BOUNCE,
+    });
 
-  //   const markerEl = marker.getElement();
+    const markerEl = marker.getElement();
 
-  //   markerEl.style.opacity = "0";
-  //   markerEl.style.transform = "translateY(-20px)";
-  //   markerEl.style.transition = "opacity 1s, transform 1s";
+    markerEl.style.opacity = "0";
+    markerEl.style.transform = "translateY(-20px)";
+    markerEl.style.transition = "opacity 1s, transform 1s";
 
-  //   const observer = new IntersectionObserver(
-  //     (entries) => {
-  //       entries.forEach((entry) => {
-  //         if (entry.isIntersecting) {
-  //           console.log("Map is in viewport");
-  //           // 여기에 원하는 동작을 추가하세요
-  //           markerEl.style.opacity = "1";
-  //           markerEl.style.transform = "translateY(0)";
-  //         }
-  //       });
-  //     },
-  //     { threshold: 0.8 } // 10%가 뷰포트에 들어오면 콜백 실행
-  //   );
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            console.log("Map is in viewport");
+            // 여기에 원하는 동작을 추가하세요
+            markerEl.style.opacity = "1";
+            markerEl.style.transform = "translateY(0)";
+          }
+        });
+      },
+      { threshold: 0.8 } // 10%가 뷰포트에 들어오면 콜백 실행
+    );
 
-  //   if (ref.current) {
-  //     observer.observe(ref.current);
-  //   }
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
 
-  //   return () => {
-  //     if (ref.current) {
-  //       observer.unobserve(ref.current);
-  //     }
-  //   };
-  // }, []);
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
 
   return (
-    <div className="w-screen h-full">
-      <div className="w-full relative flex flex-col justify-start items-center top-16">
+    <div className="w-full">
+      <div className="pt-10 relative flex flex-col justify-start items-center">
         <img src={location} className="title-location" />
         <div
           ref={ref}
           id="map"
-          className="w-full border-1 mt-13"
+          className="w-full mt-13"
           style={{ height: "100vw" }}
         ></div>
+        <div className="mt-13 p-4 ">
+          <div className="table w-full">
+            <div className="table-row">
+              <div className="table-cell p-2 font-bold">대중교통</div>
+              <div className="table-cell p-2 ">
+                5호선 오목교역 7번 출구 앞 (도보 1분)
+              </div>
+            </div>
+            <div className="table-row">
+              <div className="table-cell p-2 font-bold">자가용</div>
+              <div className="table-cell p-2">
+                <div>
+                  ∙ 전용주차장{" "}
+                  <span className="bg-parking px-2 py-1">2시간 무료</span>
+                </div>
+                <div className="ml-4">: 서울 양천구 오목로 344 청학빌딩</div>
+                <div className="mt-2">
+                  ∙ 공영주차장{" "}
+                  <span className="bg-parking px-2 py-1">3시간 무료</span>
+                </div>
+                <div className="ml-4">
+                  : 서울 양천구 목동동로 298 (도보 5분)
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
